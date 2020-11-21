3be9b8b5ba33dd0733e92278934629516c71cac2
iyrenepsam <irenepsam@gmail.com>
2020-11-21 14:22:36 +0530

    sum and difference

---
+++ android/app/src/main/kotlin/com/ione/flutter_statefull_widget_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ione.flutter_statefull_widget_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+void main()=>runApp(CalculatorApp());
+
+class CalculatorApp extends StatefulWidget {
+  @override
+  _CalculatorAppState createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+ TextEditingController getnumber1=TextEditingController();
+ TextEditingController getnumber2=TextEditingController();
+ double result=0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text("CALCULATOR APP"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                controller: getnumber1,
+                decoration: InputDecoration(
+                  hintText: " ENTER A NUMBER",
+                  border: OutlineInputBorder()
+                ),
+              ),
+              SizedBox(height: 25.0),
+
+              TextField(
+                controller: getnumber2,
+                decoration: InputDecoration(
+                  hintText: "ENTER ANOTHER NUMBER",
+                  border: OutlineInputBorder()
+                ),
+              ),
+              SizedBox(height: 25.0),
+
+              RaisedButton(
+                color: Colors.blue,
+
+                onPressed: (){
+                double getn1=double.parse(getnumber1.text);
+                double getn2=double.parse(getnumber2.text);
+
+
+                setState(() {
+                  result=getn1+getn2;
+                });
+
+                print(result);
+
+              },
+                child: Text(" SUM"),
+              ),
+                    RaisedButton(
+                      color: Colors.black12,
+                      onPressed: (){
+                      double getn1=double.parse(getnumber1.text);
+                      double getn2=double.parse(getnumber2.text);
+
+                      setState(() {
+                        result=getn1-getn2;
+                      });
+
+                    },
+                    child: Text("DIFFERENCE"),
+                    ),
+
+                    Text(result.toString()),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
